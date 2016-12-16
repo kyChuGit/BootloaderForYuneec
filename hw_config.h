3b5fe1090c362df388d9665529cf3b9e3a4e1003
David Sidrane <david_s5@nscdg.com>
2016-12-16 05:02:42 -1000

    Update hw_config.h

--- hw_config.h
+++ hw_config.h
@@ -601,8 +601,7 @@
 # define BOARD_POWER_OFF                gpio_clear
 # undef  BOARD_POWER_PIN_RELEASE		/* Leave pin enabling Power - un comment to release (disable power)*/
 # define USBMFGSTRING                   "The Autopilot"
-/* "USB_FORCE_DISCONNECT" only used in TAP_V1 on account of USB pull-down resistance*/
-# define USB_FORCE_DISCONNECT			1
+# define USB_FORCE_DISCONNECT			1 /* Use to over come HW having errant 1.5K pull up on OTG_FS_DP */
 
 /****************************************************************************
  * TARGET_HW_TAP_V2
